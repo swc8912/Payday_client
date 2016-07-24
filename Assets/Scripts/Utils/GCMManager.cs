795d0cf36d8bdd3742f478a18a7753fe2be264c2
swc8912 <swc8912@gmail.com>
2016-07-24 10:39:55 +0900

    GCMManager 추가

---
+++ Assets/Scripts/Utils/GCMManager.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class GCMManager : MonoBehaviour {
+    private string[] SENDER_IDS = { "639151196864" };
+
+	// Use this for initialization
+	void Start () {
+        // Create receiver game object
+        GCM.Initialize();
+        Debug.Log("gcm start");
+        // Set callbacks
+        GCM.SetErrorCallback((string errorId) =>
+        {
+            Debug.Log("Error!!! " + errorId);
+        });
+
+        GCM.SetMessageCallback((Dictionary<string, object> table) =>
+        {
+            Debug.Log("Message!!! " + table.ToString());
+        });
+
+        GCM.SetRegisteredCallback((string registrationId) =>
+        {
+            Debug.Log("Registered!!! " + registrationId);
+        });
+
+        GCM.SetUnregisteredCallback((string registrationId) =>
+        {
+            Debug.Log("Unregistered!!! " + registrationId);
+        });
+
+        GCM.SetDeleteMessagesCallback((int total) =>
+        {
+            Debug.Log("DeleteMessages!!! " + total);
+        });
+
+        GCM.Register(SENDER_IDS);
+	}
+}
